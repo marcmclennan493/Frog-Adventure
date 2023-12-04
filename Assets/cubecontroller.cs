13bdadfe65ac17a66ed6a6cf9a0808a8151657ff
marcmclennan493 <marcmclennan413@gmail.com>
2023-12-04 14:06:17 -0500

    test

--- Assets/cubecontroller.cs
+++ Assets/cubecontroller.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+    [RequireComponent(typeof(Rigidbody))]
 public class cubecontroller : MonoBehaviour
 {
 	
 	[SerializeField] float moveSpeed = 4;
+
+
 	
 	private playerinputmanager input;
 	private CharacterController controller;
 	private Animator animator;
-	public Rigidbody rb;
-	
+    	public Vector3 jump;
+    	public float jumpForce = 2.0f;
+    	public bool isGrounded;
+    	Rigidbody rb;
 	[SerializeField] GameObject mainCam;
 	[SerializeField] Transform cameraFollowTarget;
 	float xRotation;
@@ -20,16 +24,23 @@ public class cubecontroller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+    		rb = GetComponent<Rigidbody>();
+    		jump = new Vector3(0.0f, 2.0f, 0.0f);
         input = GetComponent<playerinputmanager>();
 		controller = GetComponent<CharacterController>();
 		animator = GetComponentInChildren<Animator>();
-		rb = GetComponent<Rigidbody>();
     }
+	
+    	void OnCollisionStay()
+    	{
+    		isGrounded = true;
+    	}
 
 
     // Update is called once per frame
     void Update()
     {
+
 		float speed = 0;
 		Vector3 inputDir = new Vector3(input.move.x, 0, input.move.y);
 		float targetRotation  = 0;
@@ -44,13 +55,16 @@ public class cubecontroller : MonoBehaviour
 			Quaternion rotation = Quaternion.Euler(0, targetRotation, 0);
 			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 20 * Time.deltaTime);
 		}
+		
+		    		if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+    
+    			rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+    			isGrounded = false;
+			}
 		animator.SetFloat("speed", input.move.magnitude);
 		Vector3 targetDirection = Quaternion.Euler(0, targetRotation, 0) * Vector3.forward;
         controller.Move(targetDirection * speed * Time.deltaTime);
 		
-		if(Input.GetButtonDown("Jump")) {
-		rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
-		}
 		
     }
 	
